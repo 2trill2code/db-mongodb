d53d9e5763e2378a60addab18c4e484e3ed2eee1
2trill2code <s4007180@student.rmit.edu.au>
2024-10-16 16:10:17 +1100

    add shadcn input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import Image from "next/image";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return <main>
     <section>
       <h2>Find properties</h2>
+      <form action="">
+        <div>
+          <label htmlFor="location" className="block mb-2">Location</label>
+          <Input id="location" onChange={() => {console.log("damn peggy")}} placeholder="Enter a location"/>
+        </div>
+        <div>
+          <label htmlFor="type" className="block mb-2">Location</label>
+        </div>
+        <div>
+          <label htmlFor="bedrooms" className="block mb-2">Location</label>
+        </div>
+      </form>
     </section>
     <section>
+      <h2>stuff</h2>
     </section>
   </main>
 }
