428d8800c0d1c78b9d4fcd528551b312926982d9
2trill2code <s4007180@student.rmit.edu.au>
2024-10-31 22:04:14 +1100

    add property card and types.ts

---
+++ src/lib/type.ts
@@ -0,0 +1,7 @@
+export type PropertyListings = {
+  id: string;
+  name: string | undefined;
+  summary: string | undefined;
+  dailyPrice: number | undefined;
+  reviewScore: number | undefined;
+};
