c675628b81b53e0b4a12d23d4daad44d68e117d3
2trill2code <s4007180@student.rmit.edu.au>
2024-10-31 22:48:24 +1100

    add server dir w express and mongodb driver

---
+++ server/src/routes/listings.js
@@ -0,0 +1,16 @@
+// GET
+// get min and max bedrooms
+
+// get all property types
+
+// get all listings
+
+// get listings by {location, type, bedrooms}
+
+// get listing by id
+
+// POST
+
+// PUT
+
+// add booking to listing by id
