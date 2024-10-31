a7bb38041e755fbe79fcd5384860ab49b16ba615
2trill2code <s4007180@student.rmit.edu.au>
2024-10-31 22:47:22 +1100

    delete old structure

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-auto px-6 py-3 border border-blue-400 bg-secondary-200">
-      <p>footer</p>
-    </footer>
-  );
-}
--- src/lib/type.ts
+++
@@ -1,7 +0,0 @@
-export type PropertyListings = {
-  id: string;
-  name: string | undefined;
-  summary: string | undefined;
-  dailyPrice: number | undefined;
-  reviewScore: number | undefined;
-};
