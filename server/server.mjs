5d7f1d919f0c3cc472a17adaf6d3c0920adb4b5e
2trill2code <s4007180@student.rmit.edu.au>
2024-11-02 21:12:04 +1100

    edit server.mjs and add cors

--- server/server.mjs
+++ server/server.mjs
@@ -1,9 +1,13 @@
 import express from "express";
 import listings from "./src/routes/listings.mjs";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
